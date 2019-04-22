d66b2c1c5c694172f545042ecf4134b3acc9028e
hachir <ha1991ha@yahoo.com>
2019-04-22 09:38:44 -0600

    Constructor

---
+++ letter.js
@@ -0,0 +1,25 @@
+
+function Letter(character) {
+    this.character = character;
+    this.guessed = false;
+
+    this.display = function() {
+        if (this.guessed === true) {
+            return this.character;
+        }
+        else {
+            return "_";
+        }  
+    }
+    this.checkGuess = function(guess) {
+        if (guess === this.character) {
+            this.guessed = true;
+        }
+        else {
+            return false;
+            
+        }
+    }
+}
+
+module.exports = Letter;
